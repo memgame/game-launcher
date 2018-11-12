9db3a0d974fa2d8dd25165921f72cdd6f3cc7481
Wenish <jonas.voland@bluewin.ch>
2018-11-12 22:21:09 +0100

    change msg

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          Hey and welcome to your new project!
         </span>
         <system-information></system-information>
       </div>
