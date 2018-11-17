f6307e9d83b41896e642d371d47031c2ddc13c0e
Wenish <jonas.voland@bluewin.ch>
2018-11-17 23:41:03 +0100

    added firebase

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyBjLeNuaiLRCjYAehwq6sJKSCMxM312VIA",
+        authDomain: "game-launcher-1439e.firebaseapp.com",
+        databaseURL: "https://game-launcher-1439e.firebaseio.com",
+        projectId: "game-launcher-1439e",
+        storageBucket: "game-launcher-1439e.appspot.com",
+        messagingSenderId: "148629216769"
+    }
+}
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,15 +3,19 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import axios from 'axios'
+import firebase from 'firebase'
 
 import App from './App'
 import router from './router'
 import store from './store'
+//import { firebaseConfig } from '../../config/config'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.use(Vuetify)
 Vue.http = Vue.prototype.$http = axios
+//Vue.prototype.$firebase = firebase.initializeApp(firebaseConfig)
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
