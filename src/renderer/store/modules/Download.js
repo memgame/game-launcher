ede1660df9dece0825fc8c311c53b49e81f9e06c
John <Jonas.Voland@swisscom.com>
2018-11-27 14:14:28 +0100

    update

--- src/renderer/store/modules/Download.js
+++ src/renderer/store/modules/Download.js
@@ -1,4 +1,6 @@
+import os from 'os'
 import firebase from 'firebase'
+import download from 'download'
 
 const state = {
     currentGameVersion: null,
@@ -19,8 +21,11 @@ const actions = {
                 console.log(querySnapshot)
                 querySnapshot.forEach(function (doc) {
                     // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data())
-                    commit('SET_NEWEST_GAME_VERSION', 'v' + doc.data().version)
+                    var data = doc.data()
+                    commit('SET_NEWEST_GAME_VERSION', 'v' + data.version)
+                    download(data.windows, 'tmp').then(() => {
+                        console.log('DONE DOWNLOAD')
+                    })
                 })
             })
             .catch(function (error) {
