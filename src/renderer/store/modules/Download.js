fd88b462c3c317159148b8b70c6dce2aaa45b433
John <Jonas.Voland@swisscom.com>
2018-11-27 14:57:40 +0100

    update

--- src/renderer/store/modules/Download.js
+++ src/renderer/store/modules/Download.js
@@ -1,6 +1,7 @@
-import os from 'os'
+import fs from 'fs'
 import firebase from 'firebase'
 import download from 'download'
+import extractZip from 'extract-zip'
 
 const state = {
     currentGameVersion: null,
@@ -10,11 +11,14 @@ const state = {
 const mutations = {
     SET_NEWEST_GAME_VERSION(state, value) {
         state.newestGameVersion = value
+    },
+    SET_CURRENT_GAME_VERSION(state, value) {
+        state.currentGameVersion = value
     }
 }
 
 const actions = {
-    downloadNewestGameVersion({ commit }) {
+    downloadNewestGameVersion({ commit, state }) {
         var db = firebase.firestore()
         db.collection('gameBuilds').orderBy('version', 'desc').limit(1).get()
             .then(function (querySnapshot) {
@@ -23,8 +27,28 @@ const actions = {
                     // doc.data() is never undefined for query doc snapshots
                     var data = doc.data()
                     commit('SET_NEWEST_GAME_VERSION', 'v' + data.version)
+                    if (state.currentGameVersion == state.newestGameVersion) {
+                        //Nothing todo since current game version is up to date
+                        console.log('GAME UP TO DATE')
+                        return;
+                    }
                     download(data.windows, 'tmp').then(() => {
                         console.log('DONE DOWNLOAD')
+                        extractZip(
+                            'tmp/testBuild.zip',
+                            {
+                                dir: process.cwd() + '/game',
+                                onEntry: (entry, zipfile) => {
+                                    console.log('EXTRACT FILE')
+                                }
+                            },
+                            (err) => {
+                                if (!err) {
+                                    console.log('DONE EXTRACT')
+                                    commit('SET_CURRENT_GAME_VERSION', 'v' + data.version)
+                                }
+                            }
+                        )
                     })
                 })
             })
