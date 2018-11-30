49d0ff09bf923cd49f11207e791c061892185379
Wenish <jonas.voland@bluewin.ch>
2018-11-30 20:20:56 +0100

    update

--- src/renderer/pages/PageHome/index.vue
+++ src/renderer/pages/PageHome/index.vue
@@ -18,7 +18,16 @@
     </v-flex>
     <v-flex xs3>
       <v-container>
-        <flag/>
+        <v-layout align-center justify-center row fill-height>
+          <v-flex xs12>
+            <flag/>
+          </v-flex>
+        </v-layout>
+        <v-layout align-center justify-center row fill-height>
+          <v-flex xs12 text-xs-center>
+            <v-btn flat small>Edit</v-btn>
+          </v-flex>
+        </v-layout>
       </v-container>
     </v-flex>
   </v-layout>
