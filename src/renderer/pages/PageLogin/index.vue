e37d64f124b5ae00ee9ee5f0d5ab3ce7c468da05
John <Jonas.Voland@swisscom.com>
2018-12-03 12:04:04 +0100

    update

--- src/renderer/pages/PageLogin/index.vue
+++ src/renderer/pages/PageLogin/index.vue
@@ -1,27 +1,27 @@
 <template>
-    <div>
-        <v-layout>
-            <v-flex xs12 sm6 md3>
-                <v-text-field
-                    label="Email"
-                    v-model="email"
-                ></v-text-field>
-                <v-text-field
-                    label="Password"
-                    v-model="password"
-                    :append-icon="showPassword ? 'visibility_off' : 'visibility'"
-                    :rules="[rules.required, rules.min, rules.hasNumber]"
-                    :type="showPassword ? 'text' : 'password'"
-                    hint="At least 8 characters and a mix of letters and numbers"
-                    counter
-                    @click:append="showPassword = !showPassword"
-                ></v-text-field>
-            </v-flex>
-        </v-layout>
+  <v-layout align-center justify-center row fill-height>
+    <v-flex xs9></v-flex>
+    <v-flex xs3 text-xs-center>
+      <div v-if="!User.isLoginInProgress">
+        <v-text-field label="Email" v-model="email"></v-text-field>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+          :rules="[rules.required, rules.min, rules.hasNumber]"
+          :type="showPassword ? 'text' : 'password'"
+          hint="At least 8 characters and a mix of letters and numbers"
+          counter
+          @click:append="showPassword = !showPassword"
+        ></v-text-field>
         <v-btn color="info" @click="trySignIn">Sign In</v-btn>
         <v-btn color="error" @click="trySignUp">Sign Up</v-btn>
-        <div>{{$route.path}}</div>
-    </div>
+      </div>
+      <div v-if="User.isLoginInProgress">
+        <v-icon>cached</v-icon>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 <script src="./PageLogin.component.js"></script>
 <style src="./PageLogin.component.css" lang="css"></style>
