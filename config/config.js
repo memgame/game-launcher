0575810c302b5de4826591c607c900fe29d20cd7
Wenish <wenish@noreply.github.com>
2019-05-30 13:09:28 +0200

    update

---
+++ config/config.js
@@ -0,0 +1,29 @@
+import os from 'os'
+
+export default {
+    firebase: {
+        apiKey: "AIzaSyBjLeNuaiLRCjYAehwq6sJKSCMxM312VIA",
+        authDomain: "game-launcher-1439e.firebaseapp.com",
+        databaseURL: "https://game-launcher-1439e.firebaseio.com",
+        projectId: "game-launcher-1439e",
+        storageBucket: "game-launcher-1439e.appspot.com",
+        messagingSenderId: "148629216769"
+    },
+    rootFolder: os.homedir() + '/mem',
+    game: {
+        folderName: 'game',
+        win32: {
+            startfile: 'mem-client-game.exe'
+        },
+        darwin: {
+            startfile: 'mem-client-game.app/Contents/MacOS/mem-client-game'
+        },
+        zipFileName: 'mem-client-game.zip'
+    },
+    tmpFolderName: 'tmp',
+    server: {
+        ip: 'localhost',
+        port: '8080',
+        roomName: 'match'
+    }
+}
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBjLeNuaiLRCjYAehwq6sJKSCMxM312VIA",
-  authDomain: "game-launcher-1439e.firebaseapp.com",
-  databaseURL: "https://game-launcher-1439e.firebaseio.com",
-  projectId: "game-launcher-1439e",
-  storageBucket: "game-launcher-1439e.appspot.com",
-  messagingSenderId: "148629216769"
-}
+import config from './../../config/config'
 
 export default async ({ Vue }) => {
     console.log('firebase init')
-    Vue.prototype.$firebase = firebase.initializeApp(firebaseConfig)
+    Vue.prototype.$firebase = firebase.initializeApp(config.firebase)
 }
