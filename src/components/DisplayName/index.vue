b79693dc0b0610fce060886c2f953e23cb4a6ba9
Wenish <jonas.voland@bluewin.ch>
2019-05-31 04:47:50 +0200

    update

--- src/components/DisplayName/index.vue
+++ src/components/DisplayName/index.vue
@@ -1,32 +1,35 @@
 <template>
-    <div class="display-name-container">
-        <div v-if="!isInEditModus">
-            <div class="text-h5">
-                {{getDisplayName}}
-                <q-btn flat icon="edit" @click="openEditModus" />
-            </div>
+    <div class="q-pa-sm">
+        <div
+            v-if="!isInEditModus"
+            class="text-h5 full-height display-name"
+        >
+            {{getDisplayName}}
+            <q-btn flat icon="edit" @click="openEditModus" />
         </div>
-        <div v-if="isInEditModus">
-            <div class="row">
-                <div class="col-xs-10">
-                    <q-input
-                        label="Display Name"
-                        v-model="newDisplayName"
-                    />
-                </div>
-                <div class="col-xs-1">
-                    <q-btn flat icon="check" @click="tryChangeDisplayName" />
-                </div>
-                <div class="col-xs-1">
-                    <q-btn flat icon="close" @click="closeEditModus" />
-                </div>
+        <div 
+            v-if="isInEditModus"
+            class="row"
+        >
+            <div class="col-xs-10">
+                <q-input
+                    label="Display Name"
+                    v-model="newDisplayName"
+                    dark
+                />
+            </div>
+            <div class="col-xs-1">
+                <q-btn flat icon="check" @click="tryChangeDisplayName" />
+            </div>
+            <div class="col-xs-1">
+                <q-btn flat icon="close" @click="closeEditModus" />
             </div>
         </div>
     </div>
 </template>
 <script src="./DisplayName.component.js"></script>
 <style lang="styl" scoped>
-.display-name-container {
-    height: 56px;
+.display-name {
+    line-height: 56px;
 }
 </style>
