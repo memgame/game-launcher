cf744c997e5a6a6c9fb4e1d4e202db8d02ab46b7
Wenish <jonas.voland@bluewin.ch>
2019-06-01 20:07:26 +0200

    update

---
+++ src/store/modules/ServerSettings.js
@@ -0,0 +1,41 @@
+import config from '../../../config/config'
+
+const state = {
+    serverIp: config.server.ip,
+    port: config.server.port,
+    roomName: config.server.roomName
+}
+
+const mutations = {
+    updateServerIp (state, value) {
+        state.serverIp = value
+    },
+    updatePort (state, value) {
+        state.port = value
+    },
+    updateRoomName (state, value) {
+        state.roomName = value
+    },
+    resetServerSettings (state) {
+        state.serverIp = config.server.ip
+        state.port = config.server.port
+        state.roomName = config.server.roomName
+    }
+}
+
+const actions = {
+}
+
+
+const getters = {
+    getServerIp: (state) => state.serverIp,
+    getPort: (state) => state.port,
+    getRoomName: (state) => state.roomName
+}
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
